980564b1c52a7013be4a92e0c03dd5256e19f3eb
Pierre-Kruse <158035538+Pierre-Kruse@users.noreply.github.com>
2024-08-19 15:16:23 +0200

    TFV0.0.1

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "azurerm"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-terraform" {
+  name     = "rg-terraform01"
+  location = "West Europe"
+  tags = {
+    environment = "dev"
+    CostCenter  = "Pkrtechsub"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet-spoke" {
+  name = "vnet-spoke-2"
+  resource_group_name = azurerm_resource_group.rg-terraform.name
+  location = azurerm_resource_group.rg-terraform.location
+  address_space = ["10.64.2.0/24"]
+
+  tags = {
+  environment = "dev"
+  CostCenter  = "Pkrtechsub"
+  }
+}
