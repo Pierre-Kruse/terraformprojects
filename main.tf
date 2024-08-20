16209ac8ad0912a622fc00183d0447bdedf74881
Pierre-Kruse <158035538+Pierre-Kruse@users.noreply.github.com>
2024-08-20 11:50:29 +0200

    commit

--- main.tf
+++ main.tf
@@ -78,3 +78,31 @@ resource "azurerm_subnet_network_security_group_association" "nsg-to-subnet" {
   network_security_group_id = azurerm_network_security_group.example-nsg.id
 }
 
+resource "azurerm_public_ip" "example-pip" {
+  name                = "pip-acceptanceTest1"
+  resource_group_name = azurerm_resource_group.rg-terraform.name
+  location            = azurerm_resource_group.rg-terraform.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "dev"
+    CostCenter  = "Pkrtechsub"
+  }
+}
+
+resource "azurerm_network_interface" "example-nic" {
+  name                = "test-nic"
+  location            = azurerm_resource_group.rg-terraform.location
+  resource_group_name = azurerm_resource_group.rg-terraform.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.pkrtech-sn.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example-pip.id
+  }
+  tags = {
+    environment = "dev"
+    CostCenter  = "Pkrtechsub"
+  }
+}
